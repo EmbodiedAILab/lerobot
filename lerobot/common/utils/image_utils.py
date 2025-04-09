eb319edfb10d5a62ba9c083bc2d01c35b0ee4d51
r00538380 <ruanyifan2@huawei.com>
2025-04-09 22:11:45 +0800

    Add 5-dim image padding method

--- lerobot/common/utils/image_utils.py
+++ lerobot/common/utils/image_utils.py
@@ -1,8 +1,9 @@
+import einops
 import numpy as np
 import torch
 import torch.nn.functional as F
 
-def resize_with_pad_train(images: torch.Tensor, target_height: int, target_width: int, method=Image.BILINEAR) -> np.ndarray:
+def resize_with_pad_train(images: torch.Tensor, target_height: int, target_width: int) -> np.ndarray:
     """Replicates tf.image.resize_with_pad for multiple images using PIL. Resizes a batch of images to a target height.
 
     Args:
@@ -14,6 +15,11 @@ def resize_with_pad_train(images: torch.Tensor, target_height: int, target_width
     Returns:
         The resized images in [..., height, width, channel].
     """
+    original_shape = None
+    if len(images.shape) == 5:
+        original_shape = images.shape
+        images = images.view(-1, *original_shape[-3:])
+    
     height, width = images.shape[-2], images.shape[-1]
     if height == target_height and width == target_width:
         return images    
@@ -30,4 +36,9 @@ def resize_with_pad_train(images: torch.Tensor, target_height: int, target_width
     padding = [
         pad_width // 2, pad_width - pad_width // 2, pad_height // 2, pad_height - pad_height // 2
     ]    
-    return F.pad(images, padding, value=0)
+    images = F.pad(images, padding, value=0)
+
+    if original_shape is not None:
+        images = images.view(*original_shape[:-3], *images.shape[-3:])
+    return images
+    
